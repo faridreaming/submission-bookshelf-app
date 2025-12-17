892bdfc4eaca076d5f5233e925bbee1d4d335076
faridreaming <97384053+faridreaming@users.noreply.github.com>
2025-12-17 16:39:04 +0700

    feat: implement tab content rendering and update book list display logic

--- js/App.js
+++ js/App.js
@@ -5,6 +5,7 @@ const addBookModalTrigger = document.getElementById('addBookModalTrigger')
 addBookModalTrigger.addEventListener('click', () => {
   addBookModal.showModal()
 })
+
 const bookFormSubmitButton = document.getElementById('bookFormSubmit')
 const bookFormSubmitButtonText = bookFormSubmitButton.querySelector('span')
 const bookFormIsComplete = document.getElementById('bookFormIsComplete')
@@ -14,6 +15,16 @@ bookFormIsComplete.addEventListener('change', function () {
     : 'Belum dibaca'
 })
 
+// TEMPLATES FROM HTML
+const allBookListTemplate = document.getElementById('allBookListTemplate')
+const completeBookListTemplate = document.getElementById(
+  'completeBookListTemplate',
+)
+const incompleteBookListTemplate = document.getElementById(
+  'incompleteBookListTemplate',
+)
+const bookItemTemplate = document.getElementById('bookItemTemplate')
+
 class App {
   static instance = null
 
@@ -22,8 +33,8 @@ class App {
 
     this.tabs = [
       { name: 'all', title: 'Daftar Semua Buku' },
-      { name: 'unread', title: 'Daftar Buku Belum Dibaca' },
-      { name: 'read', title: 'Daftar Buku Sudah Dibaca' },
+      { name: 'incomplete', title: 'Daftar Buku Belum Dibaca' },
+      { name: 'complete', title: 'Daftar Buku Sudah Dibaca' },
     ]
 
     this.activeTab = this.tabs[0]
@@ -35,7 +46,10 @@ class App {
       onChange: (tabName) => this.setTab(tabName),
     })
 
+    this.tabPanel = document.querySelector('[role="tabpanel"]')
+
     this.tabManager.render(this.activeTab.name)
+    this.renderTabcontent()
 
     App.instance = this
   }
@@ -46,8 +60,51 @@ class App {
 
     this.activeTab = found
     this.tabManager.render(found.name)
+    console.log(this.activeTab)
 
-    console.log('Tab berubah ke:', found.name)
+    this.renderTabcontent()
+  }
+
+  renderTabcontent() {
+    this.tabPanel.innerHTML = ''
+
+    let selectedTemplate
+    if (this.activeTab.name === 'all') selectedTemplate = allBookListTemplate
+    else if (this.activeTab.name === 'complete')
+      selectedTemplate = completeBookListTemplate
+    else selectedTemplate = incompleteBookListTemplate
+
+    const containerClone = selectedTemplate.content.cloneNode(true)
+
+    const listContainer = containerClone.querySelector('div')
+
+    const filteredBooks = this.books.filter((book) => {
+      if (this.activeTab.name === 'all') return true
+      if (this.activeTab.name === 'complete') return book.isComplete
+      if (this.activeTab.name === 'incomplete') return !book.isComplete
+    })
+
+    filteredBooks.forEach((book) => {
+      const bookClone = bookItemTemplate.content.cloneNode(true)
+
+      bookClone.querySelector('[data-testid="bookItemTitle"]').textContent =
+        book.title
+      bookClone.querySelector('[data-testid="bookItemAuthor"]').textContent =
+        `Penulis: ${book.author}`
+      bookClone.querySelector('[data-testid="bookItemYear"]').textContent =
+        `Tahun: ${book.year}`
+
+      const completeBtn = bookClone.querySelector(
+        '[data-testid="bookItemIsCompleteButton"]',
+      )
+      completeBtn.textContent = book.isComplete
+        ? 'Belum selesai dibaca'
+        : 'Selesai dibaca'
+
+      listContainer.appendChild(bookClone)
+    })
+
+    this.tabPanel.appendChild(containerClone)
   }
 }
 
